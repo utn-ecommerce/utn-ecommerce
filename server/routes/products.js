c468c50d51a9ca08d60c942a737ff3e2e530c990
bguzmanech <bguzmane@emeal.nttdata.com>
2023-10-29 18:35:51 -0300

    se agrega productos destacados y se crea estructura final

---
+++ server/routes/products.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Definir rutas para productos
+router.get('/', (req, res) => {
+    // Obtener productos más vendidos y ofertas
+    // Devolver datos como JSON
+});
+
+// Otras rutas relacionadas con productos
+
+module.exports = router;
