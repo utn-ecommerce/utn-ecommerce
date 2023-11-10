187c3f799bc16ba3d83410b336a2998cd2ea313c
bguzmanech <bguzmane@emeal.nttdata.com>
2023-11-10 15:16:43 -0300

    productos destacados y servicios

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,17 @@
-console.log("El script main.js se está ejecutando.");
+// ...
 
-const productosDestacados = [
-    { nombre: 'Skala Bomba', precio: 6020 },
-    { nombre: 'Skala Babosa', precio: 6020 },
-    { nombre: 'Skala Botanica', precio: 4000 },
-    { nombre: 'Skala Genetiqs', precio: 4500 }
-];
+function agregarAlCarritoDesdeDestacados(index) {
+    const producto = productosDestacados[index];
+    const productoEnCarrito = carrito.find(item => item.nombre === producto.nombre);
 
-function crearProductoDestacadoHTML(producto, index) {
-    return `
-        <div class="col mb-5">
-            <div class="card h-100">
-                <img class="card-img-top" src="../media/${producto.nombre.toLowerCase().replace(/ /g, '-')}.jpg" alt="${producto.nombre}" />
-                <div class="card-body p-4">
-                    <div class="text-center">
-                        <h5 class="fw-bolder">${producto.nombre}</h5>
-                        $${producto.precio}
-                    </div>
-                </div>
-                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center">
-                        <button class="btn btn-outline-dark mt-auto" onclick="agregarAlCarrito(${index})">Agregar al carrito</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `;
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad += 1;
+    } else {
+        carrito.push({ ...producto, cantidad: 1 });
+    }
+
+    actualizarContadorCarrito();
+    actualizarCarritoDropdown();
 }
 
-const productosDestacadosContainer = document.getElementById('productos-destacados-container');
-productosDestacados.forEach((producto, index) => {
-    productosDestacadosContainer.innerHTML += crearProductoDestacadoHTML(producto, index);
-});
+// ...
---
+++ public/js/productosDestacados.js
@@ -0,0 +1,42 @@
+const productosDestacados = [
+    { nombre: 'Skala Bomba', precio: 6020 },
+    { nombre: 'Skala Babosa', precio: 6020 },
+    { nombre: 'Skala Botanica', precio: 4000 },
+    { nombre: 'Skala Genetiqs', precio: 4500 }
+];
+
+function crearProductoDestacadoHTML(producto, index) {
+    return `
+        <div class="col mb-5">
+            <div class="card h-100">
+                <img class="card-img-top" src="../media/${producto.nombre.toLowerCase().replace(/ /g, '-')}.jpg" alt="${producto.nombre}" />
+                <div class="card-body p-4">
+                    <div class="text-center">
+                        <h5 class="fw-bolder">${producto.nombre}</h5>
+                        $${producto.precio}
+                    </div>
+                </div>
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center">
+                        <button class="btn btn-outline-dark mt-auto" onclick="agregarAlCarritoDesdeDestacados(${index})">Agregar al carrito</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
+const productosDestacadosContainer = document.getElementById('productos-destacados-container');
+let rowDestacados = document.createElement('div');
+rowDestacados.className = 'row';
+
+productosDestacados.forEach((producto, index) => {
+    if (index % 4 === 0 && index !== 0) {
+        productosDestacadosContainer.appendChild(rowDestacados);
+        rowDestacados = document.createElement('div');
+        rowDestacados.className = 'row';
+    }
+
+    rowDestacados.innerHTML += crearProductoDestacadoHTML(producto, index);
+});
+
+productosDestacadosContainer.appendChild(rowDestacados);
