59765bc12974a7bc9b8acf50c8854e879ef81706
bguzmanech <bguzmane@emeal.nttdata.com>
2023-11-09 21:00:04 -0300

    se agregan productos y catalogos

--- public/js/productos.js
+++ public/js/productos.js
@@ -1,12 +1,20 @@
 const productos = [
-    { nombre: 'Cell Active', precio: 6300 },
+    { nombre: 'Lidherma Cell Active', precio: 6300 },
     { nombre: 'Lidherma Food', precio: 7500 },
     { nombre: 'Lidherma Exfoliante', precio: 7500 },
-    { nombre: 'Lidherma Hidrosomas', precio: 8500 },
-    { nombre: 'Laca Neblina Hidratante', precio: 8500 },
-
-
-
+    { nombre: 'Lidherma Hidrosomas', precio: 6400 },
+    { nombre: 'Laca Neblina Hidratante', precio: 2500 },
+    { nombre: 'Laca Gel De Limpieza', precio: 3900 },
+    { nombre: 'Laca Locion Astringente', precio: 5000 },
+    { nombre: 'Laca Crema Para Manos', precio: 3700 },
+    { nombre: 'Idraet Alpine Roses Serum', precio: 8500 },
+    { nombre: 'Idraet Alpine Roses', precio: 9700 },
+    { nombre: 'Idraet Limpiador Microexfoliante', precio: 7300 },
+    { nombre: 'Idraet Hyaluronic Serum', precio: 8500 },
+    { nombre: 'Sentida Botanica Desodorante Natural', precio: 3800 },
+    { nombre: 'Sentida Botanica Crema Hidratante', precio: 2500 },
+    { nombre: 'Sentida Botanica Shampoo Solido', precio: 3500 },
+    { nombre: 'Sentida Botanica Acondicionador Solido', precio: 3500 }
 ];
 
 function crearProductoHTML(producto, index) {
@@ -30,6 +38,17 @@ function crearProductoHTML(producto, index) {
     `;
 }
 const productosContainer = document.getElementById('productos-container');
+let row = document.createElement('div');
+row.className = 'row';
+
 productos.forEach((producto, index) => {
-    productosContainer.innerHTML += crearProductoHTML(producto, index);
+    if (index % 4 === 0 && index !== 0) {
+        productosContainer.appendChild(row);
+        row = document.createElement('div');
+        row.className = 'row';
+    }
+
+    row.innerHTML += crearProductoHTML(producto, index);
 });
+
+productosContainer.appendChild(row);
